458f554c6d70d1c086fa6cb689282567cf9ae36b
mateusramos123 <mateus.oliveira.ifc@gmail.com>
2025-03-28 11:35:10 -0300

    Inicial

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from "vue"
+
+const contador = ref(0)
+
+function incrementar() {
+  contador.value++
+}
+
+function decrementar() {
+  contador.value--
+}
+</script>
+
+<template>
+  <h1>Contador</h1>
+  <p>Valor do Contador: {{ contador }}</p>
+  <button v-on:click="incrementar">+</button>
+  <button @click="decrementar">-</button>
+</template>
+
+<style scoped>
+button {
+  background-color: cadetblue;
+  color: white;
+  cursor: pointer;
+  font-size: 16px;
+  margin: 5px;
+}
+</style>
